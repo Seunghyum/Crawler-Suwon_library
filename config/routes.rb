0442ebe27c2bcb8cf6117a5bc8aeb2ac668fff42
Moon SeungHyun <seunghyun257@naver.com>
2016-05-09 05:59:11 +0000

    Delete redundent code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   get ":controller(/:action(/:id))"
   post ":controller(/:action(/:id))"
   root:to => "home#input"
--- db/migrate/20150727041326_create_bases.rb
+++
@@ -1,9 +0,0 @@
-class CreateBases < ActiveRecord::Migration
-  def change
-    create_table :bases do |t|
-      t.string :form_input
-
-      t.timestamps null: false
-    end
-  end
-end
